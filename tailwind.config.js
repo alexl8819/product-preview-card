e8aafb38eea828183b5c91c613629175f5f9c4c3
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-02-06 21:00:09 -0800

    move color to tailwind config and use for two other grayish elements for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       colors: {
         'custom-cream': 'hsl(30, 38%, 92%)',
         'custom-verydarkblue': 'hsl(212, 21%, 14%)',
+        'custom-gray': '#8d8b97',
         'custom-green': 'rgba(61,129,104,255)'
       }
     },
