ce4a7347703cce8c120c1700c7895aacdd84c770
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-02-06 20:04:00 -0800

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'xs': '320px',
+      ...defaultTheme.screens
+    },
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'fraunces': ['Fraunces', 'sans-serif'],
+      },
+      spacing: {
+        '96': '28rem'
+      },
+      colors: {
+        'custom-cream': 'hsl(30, 38%, 92%)',
+        'custom-darkcyan': 'hsl(158, 36%, 37%)',
+        'custom-verydarkblue': 'hsl(212, 21%, 14%)',
+        'custom-green': 'rgba(61,129,104,255)'
+      }
+    },
+  },
+  plugins: [],
+}
